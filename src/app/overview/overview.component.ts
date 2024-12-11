8690a65ccc14f3ca3b188ca6be3099d6b1874177
ihebbg <ibengara7@gmail.com>
2024-12-11 11:27:29 +0100

    maj

--- src/app/overview/overview.component.ts
+++ src/app/overview/overview.component.ts
@@ -1,12 +1,26 @@
+import { NgFor, NgIf } from '@angular/common';
 import { Component } from '@angular/core';
-
 @Component({
   selector: 'app-overview',
   standalone: true,
-  imports: [],
+  imports: [NgFor, NgIf],
   templateUrl: './overview.component.html',
   styleUrl: './overview.component.css'
 })
 export class OverviewComponent {
-
+  images = [
+    'images/overview/1-overview.jpg',
+    'images/overview/2-overview.jpg',
+    'images/overview/3-overview.jpg',
+    'images/overview/4-overview.jpg', 
+  ];
+  isPreviewOpen = false;
+  selectedImageIndex = 0;
+  openPreview(index: number): void {
+    this.selectedImageIndex = index;
+    this.isPreviewOpen = true;
+  }
+  closePreview(): void {
+    this.isPreviewOpen = false;
+  }
 }
